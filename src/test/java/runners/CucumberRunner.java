071e1b8e4aacc394e7080bfe498840743d581e85
AbrorOlimov <abrorollimov@gmail.com>
2023-12-01 02:44:42 +0500

    Initial commit

---
+++ src/test/java/runners/CucumberRunner.java
@@ -0,0 +1,21 @@
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@CucumberOptions (
+        features = "src/test/resources/features",
+        glue = "stepDefinitions",
+        publish = true,
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report/report.html"
+        }
+        ,stepNotifications = true
+)
+@RunWith(Cucumber.class)
+public class CucumberRunner {
+}
